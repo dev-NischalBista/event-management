e713238a9e2fb9f833df06f5c32045482aec4525
Nischal Bista <nbista.981@gmail.com>
2025-06-12 17:00:43 +0545

    cleaned tracked files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jest_config_1 = require("jest-config");
-const config = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleFileExtensions: [...jest_config_1.defaults.moduleFileExtensions, "mts"],
-};
-exports.default = config;
